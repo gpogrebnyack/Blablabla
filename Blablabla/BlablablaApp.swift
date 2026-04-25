2786df40118485adeffa26705a365a529dbea6f6
Zhora Pogrebnyak <krendel.mag@gmail.com>
2026-04-25 13:56:22 +0500

    Initial Commit

---
+++ Blablabla/BlablablaApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlablablaApp.swift
+//  Blablabla
+//
+//  Created by Zhora Pogrebnyak on 25.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct BlablablaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
